8070c15e51d9624a6025fcd92a742e51a013e5bc
Sergei US <ustimenko.sv@gmail.com>
2022-10-05 20:29:38 +0300

    Initial commit

--- src/test/java/ru/netology/myautomationbdd/page/TransferPage.java
+++ src/test/java/ru/netology/myautomationbdd/page/TransferPage.java
@@ -10,9 +10,7 @@ public class TransferPage {
     private static final String firstCardNumber = "5559 0000 0000 0001";
     private static final String secondCardNumber = "5559 0000 0000 0002";
     private final SelenideElement firstCardButton = $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0'] button");
-
     private final SelenideElement secondCardButton = $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d'] button");
-
     private final SelenideElement sumToTransfer = $("[data-test-id=amount] input");
     private final SelenideElement cardNumber = $("[data-test-id=from] input");
     private final SelenideElement transferButton = $("[data-test-id=action-transfer]");
