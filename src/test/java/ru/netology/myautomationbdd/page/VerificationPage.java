7f6c9c807c79031ec5a159bb14485395c84709fe
Sergei US <ustimenko.sv@gmail.com>
2022-10-07 11:08:51 +0300

    Initial commit

--- src/test/java/ru/netology/myautomationbdd/page/VerificationPage.java
+++ src/test/java/ru/netology/myautomationbdd/page/VerificationPage.java
@@ -8,16 +8,16 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class VerificationPage {
 
-    private SelenideElement codeField = $("[data-test-id=code] input");
-    private SelenideElement verifyButton = $("[data-test-id=action-verify]");
+    private final SelenideElement codeField = $("[data-test-id=code] input");
+    private final SelenideElement verifyButton = $("[data-test-id=action-verify]");
 
     public VerificationPage() {
         codeField.shouldBe(visible);
     }
 
-    public DashboardPage validVerify(DataHelper.VerificationCode verificationCode) {
+    public void validVerify(DataHelper.VerificationCode verificationCode) {
         codeField.setValue(verificationCode.getCode());
         verifyButton.click();
-        return new DashboardPage();
+        new DashboardPage();
     }
 }
